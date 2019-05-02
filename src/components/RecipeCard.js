6782077a89605941a410b47a9105af91191d05dc
1-8192 <allegranzia@gmail.com>
2019-05-02 17:31:28 -0400

    did some styling

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -3,11 +3,11 @@ import React from 'react'
 const RecipeCard = (props) => {
   return (
     <div>
-      <h3>{props.recipe.name}</h3>
-      <img className="sushi-card" src={props.recipe.img} />
-      <p>{props.recipe.roll_type}</p>
-      <p>{props.recipe.instructions}</p>
-    </div>
+          <h3>{props.recipe.name}</h3>
+          <img className="sushi-card zoom" src={props.recipe.img} />
+          <p>{props.recipe.roll_type}</p>
+          <p>{props.recipe.instructions}</p>
+          </div>
   )
 }
 
